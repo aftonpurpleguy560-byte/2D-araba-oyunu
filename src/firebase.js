13520440b61c73f68f80b9ac61c719f1509c3776
aftonpurpleguy560-byte <aftonpurpleguy560@gmail.com>
2026-02-05 23:26:45 +0300

    Update Firebase configuration with new project details

--- src/firebase.js
+++ src/firebase.js
@@ -2,14 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "SENİN_API_KEY",
-  authDomain: "SENİN_PROJECT.firebaseapp.com",
-  projectId: "SENİN_PROJECT_ID",
-  storageBucket: "SENİN_PROJECT.appspot.com",
-  messagingSenderId: "SENİN_ID",
-  appId: "SENİN_APP_ID"
+  apiKey: "AIzaSyBEAp8QJ99N-S2I74Zd3Gq-I4LcdmqYKX4",
+  authDomain: "d-araba-oyunu.firebaseapp.com",
+  projectId: "d-araba-oyunu",
+  storageBucket: "d-araba-oyunu.firebasestorage.app",
+  messagingSenderId: "480244168513",
+  appId: "1:480244168513:web:a6415e835fd2b4656f53c0"
 };
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
-
